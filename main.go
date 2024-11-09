5faa4b9af3ad8ad0dc77a634e0780241d448d6e8
Tamim Hossain <132823494+CodeWithTamim@users.noreply.github.com>
2024-11-10 00:06:54 +0600

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
-
+/*
+Add imports
+*/
 import (
 	"fmt"
 	"log"
@@ -7,10 +9,15 @@ import (
 )
 
 func main() {
+	//create a fileServer (type :Handler) and init
 	fileServer := http.FileServer(http.Dir("./static"))
+	//set the static folder as root
 	http.Handle("/", fileServer)
+	//Print starting
 	fmt.Println("Starting server on port : 8080")
+	//start server 
 	err := http.ListenAndServe(":8080", nil)
+	//check the error , if contains then log
 	if err != nil {
 		log.Fatal(err)
 	}
